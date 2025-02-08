f1a5039eeeda959e5e000cc8d9a30f23f8488da6
laylaajorge <JRGLAY001@myuct.ac.za>
2025-02-08 15:02:27 +0200

    Practical including function

--- assignment.R
+++ assignment.R
@@ -37,12 +37,35 @@ xmatrix <- cbind(1,as.matrix(xvalues))##the 1s allows us to include the intercep
 betas <- solve(t(xmatrix)%*%xmatrix)%*%t(xmatrix)%*%ymatrix
 betas
 
-f <- function(x,y){
-  beta = solve(t(x)%*%x)%*%t(x)%*%y
-  
+f <- function(xvalues,yvalues){
+ 
+  y <- as.matrix(yvalues)
+  x <- cbind(1,as.matrix(xvalues))
+  beta <- solve(t(x)%*%x)%*%t(x)%*%y
+  n <- nrow(y)
+  k <- ncol(x)
+  residual <- (y-x%*%beta)#how far the precited values are from actual values
+  fraction <- 1/(n-k)
+  rss <-sum(residual^2)
+  s <- sqrt(rss*fraction)
+  varres <- s^2*solve(t(x)%*%x)
+  se <- sqrt(diag(varres))
+  return(list("coefficents"=beta,"std.error"=se))
 }
+modelcalc <- f(xvalues,yvalues)
+modelcalc
+
+summary(modelfit)[['coefficents']][,'Std error']
 #======================================
 #Simple linear model using lm()
 #======================================
 modelfit <- lm(dist~speed,data=cars)
 summary(modelfit)
+lmcoe <- coef(modelfit)
+lmcoe
+lmse <- summary(modelfit)$coefficients[, 2]
+lmse
+calcoe <-modelcalc$coefficents
+calcoe 
+calstd <- modelcalc$std.error
+calstd
